0eee9c4a8c65f8b5d0d315f48e7210b9420f1448
Med Dah <meddahtn@gmail.com>
2019-08-04 20:14:00 +0100

    Add scoop

--- scoop.js
+++ scoop.js
@@ -2,13 +2,12 @@ const request=require("request-promise");
 const cheerio=require("cheerio");
 
 
-
 module.exports.test1=async (a)=>{
     var data=[];
 	var post = `product_par_page=100&ordre=croissant&mot_cle=${a}`
 	var x = await  request(
 		{ method: 'POST'
-		, uri: `https://www.scoopgaming.com.tn/catalogue/recherche.php`
+		, uri: `https://www.scoop.com.tn/catalogue/recherche.php`
 		, gzip: true
 		, body : post
 		,  headers: {
@@ -21,15 +20,16 @@ module.exports.test1=async (a)=>{
 		if(response.statusCode!=200)
 			return;
         $=cheerio.load(response.body);
+
 		$("div.article").each((i,el)=>{
             pic=$(el).find('img');
             title=$(el).find('span.titre_desc');
 		  	data.push({
 		  		name:title.text(),
-		  			img:"https://www.scoopgaming.com.tn/"+pic.last().attr("src"),
-		  			url:"https://www.scoopgaming.com.tn/"+title.children().eq(0).attr("href"),
-					mark:"https://www.scoopgaming.com.tn/"+pic.first().attr("src"),
-					logo:"https://www.scoopgaming.com.tn/images/scoop-logo-tn.png",
+		  			img:"https://www.scoop.com.tn/"+pic.last().attr("src"),
+		  			url:"https://www.scoop.com.tn/"+title.children().eq(0).attr("href"),
+					mark:"https://www.scoop.com.tn/"+pic.first().attr("src"),
+					logo:"https://www.scoop.com.tn/images/scoop-logo-tn.png",
 		  			price:$(el).find(".prix_article").text(),
 		  			oldPrice:null
 		  		});

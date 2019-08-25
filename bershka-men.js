cafdf4ea8d73575bbaad607222ece646c8199409
sihamza <hamzus007jouini@gmail.com>
2019-08-25 15:22:17 +0100

    fix brshka price & joker logo

--- bershka-men.js
+++ bershka-men.js
@@ -28,8 +28,8 @@ module.exports.test1=async (a)=>{
              url: 'https://www.bershka.com/tn/' + elem['productNameEn'].toLowerCase().replace(/ /g,"-") + '-c0p' + elem['json']['id']  + '.html?colorId=' + elem['json']['colorId'] ,
             mark:"",
             logo: 'https://www.bershka.com/static/itxwebstandard/images/logo.svg?t=2019072318512321db1451745598',
-           price: elem['json']['minPrice'],
-        oldPrice: elem['json']['minOldPrice']
+           price: elem['json']['minPrice']  / 100 ,
+        oldPrice: elem['json']['minOldPrice'] / 100
             });
       });
 	}).catch(function (err) {

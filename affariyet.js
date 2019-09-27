5d6775f57fdcbfbec9fcd38611c8184d76fdc13b
sihamza <hamzus007jouini@gmail.com>
2019-09-27 21:57:39 +0100

    afryet logo

--- affariyet.js
+++ affariyet.js
@@ -26,7 +26,7 @@ module.exports.test1=async (a)=>{
 			  			img:pic.attr("src"),
 			  			url:pic.parent().attr("href"),
 						mark:"",
-						logo:"https://www.affariyet.com/img/cms/328x86xLOGO,P20AFFARIYET-01.jpg.pagespeed.ic.kBsG3KRT7N.webp",
+						logo:"https://scontent.ftun3-1.fna.fbcdn.net/v/t1.15752-9/71651424_569121003832355_9159922927372599296_n.png?_nc_cat=104&_nc_oc=AQmbpM3E0XSQWXC3C5gLt3sMxNMAIw-zEL4XlRTCUndO5Gzr8cOa6nFwniFjF2cLAPw&_nc_ht=scontent.ftun3-1.fna&oh=ca99b2bdf0ee59a20b87b744d877c1c8&oe=5DF39AD8",
 			  			price:$(el).find("span.price.product-price").text(),
 			  			oldPrice:$(el).find("span.regular-price").text()
 			  		});
@@ -36,8 +36,8 @@ module.exports.test1=async (a)=>{
 		while((--maxPage)>0){
 			options.uri=`https://www.affariyet.com/recherche?s=${a}&from-xhr&page=${maxPage+1}`;
 			arrayRequest.unshift(
-					request(options)			  
-				
+					request(options)
+
 				);
 		}
 			await Promise.all(arrayRequest)
@@ -45,7 +45,7 @@ module.exports.test1=async (a)=>{
 					 requestsData.forEach(response=>{
 								if(response.statusCode!=200)
 									return;
-									
+
 								$=cheerio.load(response.body);
 								$("article.product-miniature").each((i,el)=>{
 									namee=$(el).find('h5.product-name').text().trim();
@@ -61,16 +61,16 @@ module.exports.test1=async (a)=>{
 												  price:$(el).find("span.price.product-price").text(),
 												  oldPrice:$(el).find("span.regular-price").text()
 											  });
-									
+
 								});
-							}						
+							}
 						);
-					}).catch(error => { 
+					}).catch(error => {
 					console.log(error.message)
 				  });
-		
 
-		
+
+
 	}).catch(function (err) {
         console.log(err);
     })
